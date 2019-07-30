24c0e4c15759521a3ffd885ade592d4255a13339
Alexandr Kozhevnikov <kozhevnikov@skl-co.ru>
2019-07-30 10:03:02 +0300

    fix performance

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
 function onready(fn) {
+    let passiveSupported = false;
+    try {
+        const options = Object.defineProperty({}, "passive", {
+            get: function() {
+                passiveSupported = true;
+            }
+        });
+        window.addEventListener("test", null, options); // dummy listener for check if passive is possible
+    } catch(err) {
+        console.log('Cannot use event listeners');
+    }
+
     if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
-        fn();
+        fn(passiveSupported);
     } else {
-        document.addEventListener('DOMContentLoaded', fn);
+        document.addEventListener('DOMContentLoaded', () => { fn(passiveSupported); }, passiveSupported ? { passive: true } : false);
     }
 }
 
-onready( function () {
+onready( function (passiveSupported) {
     // prevent circle color wave effect on click
     [].forEach.call(document.querySelectorAll('a'), link => {
         link.addEventListener('mousedown', (e) => {
             e.stopPropagation();
-        })
+        }, passiveSupported ? { passive: true } : false);
         link.addEventListener('touchstart', (e) => {
             e.stopPropagation();
-        })
+        }, passiveSupported ? { passive: true } : false);
     })
 });
 
